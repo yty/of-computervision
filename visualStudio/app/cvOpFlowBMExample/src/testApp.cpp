857242782c1467b19c149a6ec6730e66d6141837
takachin <takachin@bcb81ac2-d842-0410-bc88-31c5a0e7fe5c>
2008-01-06 10:42:35 +0000

    Implemented opticalflow BM.

--- visualStudio/app/cvOpFlowBMExample/src/testApp.cpp
+++ visualStudio/app/cvOpFlowBMExample/src/testApp.cpp
@@ -1,124 +1,74 @@
 #include "testApp.h"
 
-
 //--------------------------------------------------------------
 void testApp::setup(){	 
-	vidGrabber.setVerbose(true);
-	vidGrabber.initGrabber(320,240);		// windows direct show users be careful
-											// some devices (dvcams, for example) don't 
-											// allow you to capture at this resolution. 
-											// you will need to check, and if necessary, change 
-											// some of these values from 320x240 to whatever your camera supports
-											// most webcams, firewire cams and analog capture devices will support this resolution.
-	colorImg.allocate(320,240);
-	grayImage.allocate(320,240);
-	grayLastImage.allocate(320,240);
-	grayBg.allocate(320,240);
-	grayDiff.allocate(320,240);
-	bLearnBakground = true;
-	threshold = 80;
+    ofBackground(255,255,255);	
+	
+    cw = 320; ch = 240;
+    
+    vidGrabber.initGrabber(cw,ch);    
+    cameraImg.allocate(cw, ch);
+    grayImg.allocate(cw, ch);
+    pastImg.allocate(cw, ch);
+
+	opticalFlow.allocate(cw,ch);
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-	ofBackground(100,100,100);
-	vidGrabber.grabFrame();
-	
-	if (vidGrabber.isFrameNew()){
-		
-		colorImg.setFromPixels(vidGrabber.getPixels(), 320,240);
-		grayImage.setFromColorImage(colorImg);
-		
-		
-		if (bLearnBakground == true){
-			grayBg = grayImage;		// the = sign copys the pixels from grayImage into grayBg (operator overloading)
-			bLearnBakground = false;
-		}
-		
-		// take the abs value of the difference between background and incoming and then threshold:
-		grayDiff.absDiff(grayBg, grayImage);
-		grayDiff.threshold(threshold);
-
-		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
-		// also, find holes is set to true so we will get interior contours as well....		
-		
-		opticalFlow.calcOpticalFlowBM(grayLastImage,grayImage,10);
-		
-		grayLastImage = grayImage;
-	}
+    
+    vidGrabber.grabFrame();
+    
+    if(vidGrabber.isFrameNew()){
+        pastImg = grayImg;
+        
+        cameraImg.setFromPixels(vidGrabber.getPixels(), cw, ch);
+        grayImg.setFromColorImage(cameraImg);
+        
+        grayImg.threshold(100);
+        grayImg.blurHeavily();
+                
+        // printf("%p %p\n", pastImg.getCvImage(), grayImg.getCvImage());
+		opticalFlow.calc(pastImg, grayImg,10);		
+    }
+    
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
-	// draw the incoming, the grayscale, the bg and the thresholded difference
-	ofSetColor(0xffffff);
-	//vidGrabber.draw(20,20);
-	
-	
-	colorImg.draw(20,20);	
-	grayImage.draw(360,20);
-	grayBg.draw(20,280);
-	grayDiff.draw(360,280);
-
-	
-	
-	// then draw the contours:
-
-	ofFill();
-	ofSetColor(0x333333);
-	ofRect(360,540,320,240);
-	ofSetColor(0xffffff);
-	glPushMatrix();
-		glTranslatef(360,540,0);
-		opticalFlow.draw();
-		//contourFinder.draw();
-	glPopMatrix();
-	//
-	// finally, a report:
-
-	ofSetColor(0xffffff);
-	char reportStr[1024];
-	sprintf(reportStr, "bg subtraction and blob detection\npress ' ' to capture bg\nthreshold %i (press: +/-)\nnum blobs found %i", threshold, contourFinder.nBlobs);
-	ofDrawBitmapString(reportStr, 20, 600);
-
+    cameraImg.draw(0,0);
+    grayImg.draw(320,0);
+    pastImg.draw(0,240);
+	opticalFlow.draw();       
 }
 
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){ 
+	if(key == 's'){
+        vidGrabber.videoSettings();
+    }
+}
 
 //--------------------------------------------------------------
-void testApp::keyPressed  (int key){ 
+void testApp::keyReleased(int key){ 
 	
-	switch (key){
-		case 's':
-			vidGrabber.videoSettings();
-			break;
-		case ' ':
-			bLearnBakground = true;
-			break;
-		case '+':
-			threshold ++;
-			if (threshold > 255) threshold = 255;
-			break;
-		case '-':
-			threshold --;
-			if (threshold < 0) threshold = 0;
-			break;
-	}
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
-}	
+	
+}
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
+	
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
+
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(){
-
 }
