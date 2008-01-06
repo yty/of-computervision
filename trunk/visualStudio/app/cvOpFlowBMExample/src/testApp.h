1d5c41e5dc2b22ea0adbae821ec1bbac27cf1943
takachin <takachin@bcb81ac2-d842-0410-bc88-31c5a0e7fe5c>
2008-01-06 10:42:35 +0000

    Implemented opticalflow BM.

--- trunk/visualStudio/app/cvOpFlowBMExample/src/testApp.h
+++ trunk/visualStudio/app/cvOpFlowBMExample/src/testApp.h
@@ -1,38 +1,43 @@
 #ifndef _TEST_APP
 #define _TEST_APP
 
+
 #include "ofMain.h"
 #include "ofCvMain.h"
 
+
 class testApp : public ofSimpleApp{
-
+	
 	public:
-
+		
 		void setup();
 		void update();
 		void draw();
-
-		void keyPressed  (int key);
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
 		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
 		void mouseReleased();
-
-		ofVideoGrabber 		vidGrabber;
-		ofCvGrayscaleImage 	grayImage;
-		ofCvGrayscaleImage 	grayLastImage;
-		ofCvGrayscaleImage 	grayBg;
-		ofCvGrayscaleImage 	grayDiff;
-		ofCvColorImage		colorImg;
-
-		ofCvContourFinder 	contourFinder;
-
-		ofCvOpticalFlow		opticalFlow;
-
-		int 				threshold;
-		bool				bLearnBakground;
-
-
+        
+        ofCvColorImage cameraImg;
+        ofCvGrayscaleImage grayImg;
+        ofCvGrayscaleImage pastImg;
+		ofCvOpticalFlowBM	opticalFlow;
+        
+        int rows, cols;
+        int block_size;
+        int shift_size;
+        int cw, ch;
+        
+        CvMat *velx, *vely;
+        CvSize block;
+        CvSize shift;
+        CvSize max_range;
+        
+        ofVideoGrabber vidGrabber;
 };
 
-#endif
+#endif	
+
